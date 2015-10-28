70606c6b9994e391f3022c4b63a4cf2858009088
Micheal Parks <michealparks1989@gmail.com>
2015-10-28 19:02:02 -0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # canvas-starfield
+
 A canvas rendered starfield.
+
+### Install
+
+```bash
+npm install canvas-starfield
+```
+
+### Getting Started
+
+```javascript
+
+var Starfield = require('canvas-starfield')
+var sf = new Starfield({
+  canvas: '#starfield-canvas', // string selector or HTML element
+  numStars: 800,               // default 500
+  dx: 0.05,                    // x speed of stars in px/frame, default 0.05
+  dy: 0.025,                   // y speed of stars in px/frame, default 0.05
+  maxRadius: 2                 // maximum star radius in px
+})
+
+sf.start()
+
+```
---
+++ canvas-starfield.js
@@ -0,0 +1,108 @@
+/*
+
+The MIT License (MIT)
+
+Copyright (c) 2015 Micheal Parks
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+(function(window, sf) {
+
+  if (typeof module !== 'undefined') module.exports = sf
+  else window.Starfield = sf 
+
+})(this, function() {
+
+'use strict'
+
+/*
+ * config:
+ * {
+ *   canvas: string or HTML element
+ *   numStars: number, default 500
+ *   vx: number, rate of x movement, default 0.05
+ *   vy: number, rate of y movement, default 0.05
+ *   maxRadius: number, default 2
+ * }
+ */
+function Starfield(config) {
+  this.canvas = typeof config.canvas === 'string' ?
+    document.querySelector(config.canvas) :
+    config.canvas
+  this.ctx = this.canvas.getContext('2d')
+  this.style = getComputedStyle(this.canvas)
+  
+  this.onResize()
+
+  this.stars = []
+  this.vx = config.vx || 0.05
+  this.vy = config.vy || 0.05
+
+  for (var i = 0, l = config.numStars || 500; i < l; i++) this.stars.push({
+    x: Math.round(Math.random() * this.canvas.width), 
+    y: Math.round(Math.random() * this.canvas.height),
+    r: 0.5 + (Math.random() * (config.maxRadius || 500)),
+    l: Math.random(),
+    bl: 0.1 * (Math.random()*6 + 2),
+    dl: Math.round(Math.random()) === 1? 0.01: -0.01
+  })
+
+  window.addEventListener('resize', this.onResize.bind(this))
+}
+
+Starfield.prototype.onResize = function() {
+  this.canvas.width = Number(this.style.width.replace('px', '')) * window.devicePixelRatio
+  this.canvas.height = Number(this.style.height.replace('px', '')) * window.devicePixelRatio
+}
+
+Starfield.prototype.start = function() {
+  var tick = function() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    this.ctx.fillStyle = 'black' 
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
+
+    var i = this.stars.length
+    while (i-- > 0) {
+      var star = this.stars[i]
+      this.ctx.beginPath()
+      this.ctx.fillStyle = 'rgba(255,255,255,' + star.l + ')' 
+      this.ctx.arc(star.x, star.y, star.r, 0, 2 * Math.PI, false)
+      this.ctx.fill()
+      
+      star.y += this.vy 
+      star.x += this.vx
+      star.l += star.dl
+
+      if (Math.abs(star.l - star.bl) >= 0.25) star.dl = -star.dl
+      if (star.x > this.canvas.width) star.x = this.vx > 0? -1: this.canvas.width + 1
+      if (star.y > this.canvas.height) star.y = this.vy > 0? -1: this.canvas.height + 1
+    }
+
+    this.frameId = window.requestAnimationFrame(tick)
+
+  }.bind(this)
+  
+  this.frameId = window.requestAnimationFrame(tick)
+}
+
+return Starfield
+
+})
